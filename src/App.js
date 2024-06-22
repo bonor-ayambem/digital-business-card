79cca272e695b40d0cbd4d5e6aa3564194e0dc64
Bonor Ayambem <bayambem@guidewire.com>
2024-06-22 18:19:20 -0400

    most text completed

--- src/App.js
+++ src/App.js
@@ -9,20 +9,27 @@ export default function App() {
         <h1>Bonor Ayambem</h1>
         <h3>Software Engineer</h3>
         <h4>Bethlehem, PA</h4>
-        <div className='social-buttons'>
-          <button>Email</button>
-          <button>LinkedIn</button>
-        </div>
+      </div>
+      <div className='social-buttons'>
+          <button className='email-btn'>Email</button>
+          <button className='linkedin-btn'>LinkedIn</button>
       </div>
       <div className='text-content'>
         <h2 className='about'>About</h2>
-        <p>Bonor Ayambem is</p>
+        <p className='about-txt'>Bonor Ayambem is a software engineer originally from Abuja, Nigeria.
+          She attended Lehigh University from 2019 to 2024, where she obtained both
+          her Bachelor's and Master's degree in Computer Science. She has worked as a software engineer
+          intern at Jacobs Engineering Group and Guidewire Software, and studied Computer Vision in 
+          graduate school.
+        </p>
         <h2>Interests</h2>
-        <p>Placeholder</p>
-        <footer>
+        <p className='interests-txt'>Bonor is a runner and dancer. She is also interested in fashion and beauty, finance. 
+          She has recently been learning American Sign Language, and enjoys to read and write.
+        </p>
+      </div>
+      <footer>
           <p>placeholder</p>
         </footer>
-      </div>
     </div>
   )
 }
