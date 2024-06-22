f65a3d468ac42482a301a7c2cb9830cf91201ca2
Bonor Ayambem <bayambem@guidewire.com>
2024-06-22 17:53:21 -0400

    placeholders

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ export default function App() {
   return (
     <div className='card'>
       <img src={pic} alt="profile-picture" className='profile-pic'/>
-      <h1>Bonor Ayambem</h1>
+      <div className='header'>
+        <h1>Bonor Ayambem</h1>
+        <h3>Software Engineer</h3>
+        <h4>Bethlehem, PA</h4>
+        <div className='social-buttons'>
+          <button>Email</button>
+          <button>LinkedIn</button>
+        </div>
+      </div>
+      <div className='text-content'>
+        <h2 className='about'>About</h2>
+        <p>Bonor Ayambem is</p>
+        <h2>Interests</h2>
+        <p>Placeholder</p>
+        <footer>
+          <p>placeholder</p>
+        </footer>
+      </div>
     </div>
   )
 }
